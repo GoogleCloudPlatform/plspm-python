49dd2196ed4c4f810fd7e9bd37c4b68e0cbf0907
Jez Humble <jez@jezhumble.net>
2019-02-18 18:32:06 -0800

    Verifying the rest of the metric model output

--- tests/test_regression_metric.py
+++ tests/test_regression_metric.py
@@ -34,7 +34,7 @@ def satisfaction_path_matrix():
 
 
 def test_plspm_satisfaction():
-    russa = pd.read_csv("file:tests/data/satisfaction.csv", index_col=0)
+    satisfaction = pd.read_csv("file:tests/data/satisfaction.csv", index_col=0)
     config = c.Config(satisfaction_path_matrix(), scaled=False)
     config.add_lv("IMAG", mode.A, c.MV("imag1"), c.MV("imag2"), c.MV("imag3"), c.MV("imag4"), c.MV("imag5"))
     config.add_lv("EXPE", mode.A, c.MV("expe1"), c.MV("expe2"), c.MV("expe3"), c.MV("expe4"), c.MV("expe5"))
@@ -43,7 +43,7 @@ def test_plspm_satisfaction():
     config.add_lv("SAT", mode.A, c.MV("sat1"), c.MV("sat2"), c.MV("sat3"), c.MV("sat4"))
     config.add_lv("LOY", mode.A, c.MV("loy1"), c.MV("loy2"), c.MV("loy3"), c.MV("loy4"))
 
-    plspm_calc = Plspm(russa, config)
+    plspm_calc = Plspm(satisfaction, config)
     expected_scores = pd.read_csv("file:tests/data/satisfaction.scores.csv")
     npt.assert_allclose(util.sort_cols(expected_scores), util.sort_cols(plspm_calc.scores()))
 
@@ -52,33 +52,31 @@ def test_plspm_satisfaction():
     npt.assert_allclose(util.sort_cols(expected_inner_model).sort_index(),
                         util.sort_cols(actual_inner_model).sort_index())
     expected_outer_model = pd.read_csv("file:tests/data/satisfaction.outer-model.csv", index_col=0)
-    print(plspm_calc.outer_model())
     npt.assert_allclose(
-        util.sort_cols(expected_outer_model.filter(["weight", "loading", "communality", "redundancy"])).sort_index(),
+        util.sort_cols(expected_outer_model.drop(["block"], axis=1)).sort_index(),
         util.sort_cols(plspm_calc.outer_model()).sort_index())
 
-#    expected_crossloadings = pd.read_csv("file:tests/data/russa.crossloadings.csv", index_col=0)
-#    npt.assert_allclose(util.sort_cols(expected_crossloadings.filter(["AGRI", "IND", "POLINS"])).sort_index(),
-#                        util.sort_cols(plspm_calc.crossloadings()).sort_index())
+    expected_crossloadings = pd.read_csv("file:tests/data/satisfaction.crossloadings.csv", index_col=0)
+    npt.assert_allclose(util.sort_cols(expected_crossloadings.drop(["block"], axis=1)).sort_index(),
+                        util.sort_cols(plspm_calc.crossloadings()).sort_index())
 
-#    expected_inner_summary = pd.read_csv("file:tests/data/russa.inner_summary.csv", index_col=0)
-#    npt.assert_allclose(util.sort_cols(
-#        expected_inner_summary.filter(["r_squared", "block_communality", "mean_redundancy", "ave"])).sort_index(),
-#                        util.sort_cols(plspm_calc.inner_summary().filter(
-#                            ["r_squared", "block_communality", "mean_redundancy", "ave"])).sort_index())
-#    pt.assert_series_equal(expected_inner_summary.loc[:, "type"].sort_index(),
-#                           plspm_calc.inner_summary().loc[:, "type"].sort_index())
+    expected_inner_summary = pd.read_csv("file:tests/data/satisfaction.inner-summary.csv", index_col=0)
 
-#    plspm_calc_path = Plspm(russa, config, scheme.PATH, 100, 0.0000001)
-#    expected_outer_model_path = util.sort_cols(
-#        pd.read_csv("file:tests/data/russa.outer_model_path.csv", index_col=0).filter(
-#            ["weight", "loading", "communality", "redundancy"])).sort_index()
-#    npt.assert_allclose(expected_outer_model_path,
-#                        util.sort_cols(plspm_calc_path.outer_model()).sort_index())
+    npt.assert_allclose(util.sort_cols(expected_inner_summary.drop(["type"], axis=1)).sort_index(),
+                        util.sort_cols(plspm_calc.inner_summary().drop(["type"], axis=1)).sort_index())
+    pt.assert_series_equal(expected_inner_summary.loc[:, "type"].sort_index(),
+                           plspm_calc.inner_summary().loc[:, "type"].sort_index())
 
-#    plspm_calc_factorial = Plspm(russa, config, scheme.FACTORIAL, 100, 0.0000001)
-#    expected_outer_model_factorial = util.sort_cols(
-#        pd.read_csv("file:tests/data/russa.outer_model_factorial.csv", index_col=0).filter(
-#            ["weight", "loading", "communality", "redundancy"])).sort_index()
-#    npt.assert_allclose(expected_outer_model_factorial,
-#                        util.sort_cols(plspm_calc_factorial.outer_model()).sort_index())
+    plspm_calc_path = Plspm(satisfaction, config, scheme.PATH)
+    expected_outer_model_path = util.sort_cols(
+        pd.read_csv("file:tests/data/satisfaction.outer-model-path.csv", index_col=0).drop(["block"],
+                                                                                           axis=1)).sort_index()
+    npt.assert_allclose(expected_outer_model_path,
+                        util.sort_cols(plspm_calc_path.outer_model()).sort_index())
+
+    plspm_calc_factorial = Plspm(satisfaction, config, scheme.FACTORIAL)
+    expected_outer_model_factorial = util.sort_cols(
+        pd.read_csv("file:tests/data/satisfaction.outer-model-factorial.csv", index_col=0).drop(["block"],
+                                                                                                axis=1)).sort_index()
+    npt.assert_allclose(expected_outer_model_factorial,
+                        util.sort_cols(plspm_calc_factorial.outer_model()).sort_index())
