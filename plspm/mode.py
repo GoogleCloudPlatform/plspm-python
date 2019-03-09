Fixed calculation for nonmetric data with missing observations

plspm/mode.py
@@ -31,7 +31,7 @@ class _ModeA:
             weights = np.nansum(mv_grouped_by_lv[lv] * Z[:, np.newaxis], axis=0)
             weights = weights / np.sum(np.power(mv_grouped_by_lv_missing[lv] * Z[:, np.newaxis], 2), axis=0)
             Y = np.nansum(np.transpose(mv_grouped_by_lv[lv]) * weights[:, np.newaxis], axis=0)
-            Y = Y / np.sum(np.power(np.transpose(mv_grouped_by_lv_missing[lv]) * weights[:, np.newaxis], 2))
+            Y = Y / np.sum(np.power(np.transpose(mv_grouped_by_lv_missing[lv]) * weights[:, np.newaxis], 2), axis=0)
         else:
             weights = np.dot(np.transpose(mv_grouped_by_lv[lv]), Z) / np.power(Z, 2).sum()
             Y = np.dot(mv_grouped_by_lv[lv], weights)

plspm/scale.py
@@ -22,7 +22,9 @@ from enum import Enum
 class _Numeric:
 
     def scale(self, lv: str, mv: str, z_by_lv: pd.Series, weights) -> pd.DataFrame:
-        return util.treat_numpy(weights.mv_grouped_by_lv(lv, mv)) * weights.correction()
+        data = weights.mv_grouped_by_lv(lv, mv)
+        finite_elements = np.isfinite(data).sum()
+        return util.treat_numpy(data) * np.sqrt(finite_elements / (finite_elements - 1))
 
 
 class _Raw: