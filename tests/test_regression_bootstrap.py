e019c1463a8eb78fa59d641c82cec6393ffdccc0
Jez Humble <jez@jezhumble.net>
2020-06-23 18:55:25 -0700

    Fixed bootstrapping test now we have a new column

--- tests/test_regression_bootstrap.py
+++ tests/test_regression_bootstrap.py
@@ -19,6 +19,8 @@ def satisfaction_path_matrix():
 
 def test_bootstrap_metric():
     satisfaction = pd.read_csv("file:tests/data/satisfaction.csv", index_col=0)
+    columns_to_drop = ["t stat."]
+    
     config = c.Config(satisfaction_path_matrix(), scaled=False)
     config.add_lv_with_columns_named("IMAG", Mode.A, satisfaction, "imag")
     config.add_lv_with_columns_named("EXPE", Mode.A, satisfaction, "expe")
@@ -30,20 +32,20 @@ def test_bootstrap_metric():
     plspm_calc = Plspm(satisfaction, config, bootstrap=True)
     expected_boot_weights = pd.read_csv("file:tests/data/satisfaction_boot_weights.csv", index_col=0)
     npt.assert_allclose(util.sort_cols(expected_boot_weights),
-                        util.sort_cols(plspm_calc.bootstrap().weights()), rtol=0.5)
+                        util.sort_cols(plspm_calc.bootstrap().weights().drop(columns=columns_to_drop)), rtol=0.5)
 
     expected_boot_rsquared = pd.read_csv("file:tests/data/satisfaction_boot_rsquared.csv", index_col=0)
     npt.assert_allclose(util.sort_cols(expected_boot_rsquared),
-                        util.sort_cols(plspm_calc.bootstrap().r_squared()), rtol=0.5)
+                        util.sort_cols(plspm_calc.bootstrap().r_squared().drop(columns=columns_to_drop)), rtol=0.5)
 
     expected_boot_total_effects = pd.read_csv("file:tests/data/satisfaction_boot_total_effects.csv", index_col=0)
     npt.assert_allclose(util.sort_cols(expected_boot_total_effects),
-                        util.sort_cols(plspm_calc.bootstrap().total_effects()), rtol=0.6)
+                        util.sort_cols(plspm_calc.bootstrap().total_effects().drop(columns=columns_to_drop)), rtol=0.6)
 
     expected_boot_paths = pd.read_csv("file:tests/data/satisfaction_boot_paths.csv", index_col=0)
     npt.assert_allclose(util.sort_cols(expected_boot_paths),
-                        util.sort_cols(plspm_calc.bootstrap().paths()), rtol=5)
+                        util.sort_cols(plspm_calc.bootstrap().paths().drop(columns=columns_to_drop)), rtol=5)
 
     expected_boot_loadings = pd.read_csv("file:tests/data/satisfaction_boot_loadings.csv", index_col=0)
     npt.assert_allclose(util.sort_cols(expected_boot_loadings),
-                        util.sort_cols(plspm_calc.bootstrap().loading()), rtol=0.5)
+                        util.sort_cols(plspm_calc.bootstrap().loading().drop(columns=columns_to_drop)), rtol=0.5)
